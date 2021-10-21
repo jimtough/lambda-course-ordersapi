00d69181afe2ceb55aeb9232d9a9af562d974f46
adultmalehuman <adultmalehuman@gmail.com>
2021-10-21 15:28:22 -0300

    Integrated DynamoDB with Get Orders method

--- ordersapi/src/main/java/org/jimtough/aws/lambda/apis/ReadOrdersLambda.java
+++ ordersapi/src/main/java/org/jimtough/aws/lambda/apis/ReadOrdersLambda.java
@@ -1,5 +1,12 @@
 package org.jimtough.aws.lambda.apis;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -11,9 +18,17 @@ public class ReadOrdersLambda {
 	public APIGatewayProxyResponseEvent getOrders(
 			APIGatewayProxyRequestEvent request)
 	throws JsonProcessingException {
+		AmazonDynamoDB dynamoDB = AmazonDynamoDBClientBuilder.defaultClient();
+		ScanResult scanResult = dynamoDB.scan(new ScanRequest().withTableName(System.getenv("ORDERS_TABLE")));
+		List<Order> orders =
+			scanResult.getItems().stream()
+				.map(item->new Order(
+					Integer.parseInt(item.get("id").getN()),
+				    item.get("itemName").getS(),
+				    Integer.parseInt(item.get("quantity").getN())))
+				.collect(Collectors.toList());
 		ObjectMapper objectMapper = new ObjectMapper();
-		Order order = new Order(123,"what ever", 111);
-		String jsonOutput = objectMapper.writeValueAsString(order);
+		String jsonOutput = objectMapper.writeValueAsString(orders);
 		return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody(jsonOutput);
 	}
 
